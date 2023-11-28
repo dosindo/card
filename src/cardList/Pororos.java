8b98570fc6274814f000f594b2270941df8b8bd1
dosindo <cjm5281@naver.com>
2023-11-28 10:42:42 +0900

    거의다함

--- src/cardList/Pororos.java
+++ src/cardList/Pororos.java
@@ -43,25 +43,10 @@ public class Pororos extends Card {
         player.decreaseHealth(ad);
         System.out.println("공격을 해땅 " + getAd());
     }
-    public void attack(Player player,Enemyfield enemyfield) {
-        setatm(new Music(getAtmname(),false));
-        setAtmname("deck1.mp3");
-        getAtm().start();
 
-        boolean directattck = true;
-        for(int i=0;i<enemyfield.getfieldsize();i++){
-            if(this.getFieldnum()==enemyfield.field.get(i).fieldnum){
-                int eneHp = enemyfield.field.get(i).getHp();
-                eneHp-=getAd();
-                enemyfield.field.get(i).setHp(eneHp);
-                directattck=false;
 
-            }
-        }
-        if(directattck) {
-            player.decreaseHealth(getAd());
-        }
-        player.decreaseHealth(2);
+    @Override
+    public void mainEffect(Field field, Enemyfield enemyfield) {
+        getPlayer2().setHealth(getPlayer2().getHealth()-2);
     }
-
 }
